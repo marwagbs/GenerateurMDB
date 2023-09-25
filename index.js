14b03437237f347d538a1bdca31efe690df9072f
marwagbs <90385255+marwagbs@users.noreply.github.com>
2023-09-25 10:47:59 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const dataLowercase="azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase=dataLowercase.toUpperCase();
+const dataNumbers="0123456789";
+const dataSymbols="^$ù%,;:!§/\"'(-è_ç)";
+const rangeValue=document.getElementById("password-length");
+const passwordOutput=document.getElementById("password-output");
+
+function generatePassword(){
+   let data=[];
+   let password="";
+   //le spreed opérateur pour diviser la chaine
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (numbers.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
+
+    if(data.length===0){
+        alert("Veuillez sélectionner des critères")
+        return;
+    }
+    for(i=0; i< rangeValue.value ; i++){
+        password += data[Math.floor(Math.random()* data.length)]
+        
+    }
+    passwordOutput.value=password;
+    passwordOutput.select();
+    navigator.clipboard.writeText(passwordOutput.value);
+
+
+    generateButton.textContent="copié !"
+
+    setTimeout(()=>{
+        generateButton.textContent="Générer mot de passe"
+    },2000)
+}
+
+generateButton.addEventListener("click",generatePassword)
+
